81f683ddf5c28fcf0e8760f0d13021b6bd52c059
Pranav-ElastikTeams <pranav.bharti@elastikteams.com>
2025-03-30 21:21:09 +0530

    Initial commit

---
+++ admins/includes/footer.php
@@ -0,0 +1,5 @@
+<div class="copyrights">
+	 <p><u><a href="https://www.agrongo.com/">AgroNGO</a></u> is a project company for Farmers and Consumers</p>
+                    <p class="h6">Developed in 2025. All rights reserved.</p>
+
+</div>	
